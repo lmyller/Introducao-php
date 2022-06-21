fa36966046f94a6f8bb436b67f71d2c53c33e690
Myller <myller42@gmail.com>
2022-06-21 12:44:27 -0300

    Inicio php

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
-echo 'Isso é um teste';
+/* Declarando variáveis */
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+ 
+$nome = 'Eduardo';
+$idade = 13;
+
+/* Verificar tipo de dados */
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <= 12)
+    echo 'infantil';
+
+else if($idade >= 13 && $idade <= 17)
+    echo 'adolescente';
+
+else
+    echo 'adulto';
+
+
+    
