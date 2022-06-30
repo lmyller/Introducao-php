925c51ccc2b027a355688185fafd143e65841994
Myller <myller42@gmail.com>
2022-06-30 14:43:15 -0300

    Adição de "loop for"

--- script.php
+++ script.php
@@ -5,7 +5,6 @@ $categorias = [];
 $categorias[] = 'infantil';
 $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
-$categorias[] = 'idoso';
  
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
